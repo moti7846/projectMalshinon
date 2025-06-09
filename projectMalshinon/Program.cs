57b4814b887a23d6c0c368dd11aa57f674e966d6
moti7846 <8563816@gmail.com>
2025-06-09 17:42:04 +0300

    finish day 9/6

--- projectMalshinon/Program.cs
+++ projectMalshinon/Program.cs
@@ -8,8 +8,43 @@ namespace projectMalshinon
 {
     internal class Program
     {
+        public static void test(string str)
+        {
+            string Fname = null;
+            string lname = null;
+
+            string[] parts = str.Split(' ');
+            foreach (string s in parts)
+            {
+                if (char.IsUpper(s[0]))
+                {
+                    if(Fname == null)
+                    {
+                        Fname = s;
+                    }
+                    else if(lname == null)
+                    {
+                        lname = s;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(Fname);
+            Console.WriteLine(lname);
+            Console.WriteLine(str);
+
+        }
+
+
         static void Main(string[] args)
         {
+            //report a = report.GetInstance();
+            //a.searchThePeopleTable("moti", "moiz");
+            //a.searchThePeopleTable("moti", "moiz");
+            //a.searchThePeopleTable("aaaa", "moiz");
+            //a.searchThePeopleTable("moti", "moz");
+            test("my name is Moti Minzberg i liked piza");
+
         }
     }
 }
---
+++ projectMalshinon/people.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projectMalshinon
+{
+    internal class people
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string secret_code { get; set; }
+        public string type_people { get; set; }
+        public int num_reports { get; set; }
+        public int num_mentions { get; set; }
+
+        public people(int id, string first_name, string last_name, string secret_code, string type_people, int num_reports, int num_mentions)
+        {
+            this.id = id;
+            this.first_name = first_name;
+            this.last_name = last_name;
+            this.secret_code = secret_code;
+            this.type_people = type_people;
+            this.num_reports = num_reports;
+            this.num_mentions = num_mentions;
+
+        }
+        public people(string first_name, string last_name, string secret_code, string type_people, int num_reports, int num_mentions)
+        {
+            this.first_name = first_name;
+            this.last_name = last_name;
+            this.secret_code = secret_code;
+            this.type_people = type_people;
+            this.num_reports = num_reports;
+            this.num_mentions = num_mentions;
+
+        }
+        //public static void printer(List<people> age)
+        //{
+        //    Console.WriteLine("id______CodeName______RealName______Location______agentStatus______MissionsCompleted");
+        //    foreach (people item in age)
+        //    {
+        //        Console.WriteLine($"{item.Id}       {item.CodeName}    {item.RealName}    {item.Location}         {item.agentStatus}              {item.MissionsCompleted}");
+        //    }
+        //}
+    }
+}
+
