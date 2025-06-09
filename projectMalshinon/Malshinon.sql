f40ffea9ba807de0b303771eb5b14d884fa40754
moti7846 <8563816@gmail.com>
2025-06-09 11:10:41 +0300

    first building

---
+++ projectMalshinon/Malshinon.sql
@@ -0,0 +1,29 @@
+/* Create the database */
+CREATE DATABASE  IF NOT EXISTS Malshinon;
+
+/* Switch to the classicmodels database */
+USE Malshinon;
+
+/* Create the tables */
+CREATE TABLE People 
+(
+    id INT AUTO_INCREMENT PRIMARY KEY ,
+    first_name varchar(50) UNIQUE,
+    last_name varchar(50),
+    secret_code varchar(10) UNIQUE,
+    type_people ENUM('reporter', 'target', 'both', 'potential_agent'),
+    num_reports INT DEFAULT 0,
+    num_mentions INT DEFAULT 0
+);
+
+CREATE TABLE IntelReports
+(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    reporter_ID INT,
+    target_ID INT,
+    text_report TEXT,
+    timestamp DATETIME DEFAULT NOW(),
+
+    FOREIGN KEY (reporter_ID) REFERENCES People(id),
+    FOREIGN KEY (target_ID) REFERENCES People(id)
+);
