691e5fde21660939fc3d699d21e075edc1aca243
moti7846 <8563816@gmail.com>
2025-06-12 12:54:47 +0300

    finish project

--- projectMalshinon/Alerts.cs
+++ projectMalshinon/Alerts.cs
@@ -8,6 +8,32 @@ namespace projectMalshinon
 {
     internal class Alerts
     {
+        public int id { get; set; }
+        public int target_ID { get; set; }
+        public DateTime created_at { get; set; }
+        public string reason { get; set; }
 
+        public Alerts(int id, int target_ID, DateTime created_at, string reason)
+        {
+            this.id = id;
+            this.target_ID = target_ID;
+            this.created_at = created_at;
+            this.reason = reason;
+        }
+        public Alerts(int target_ID, DateTime created_at, string reason)
+        {
+            this.target_ID = target_ID;
+            this.created_at = created_at;
+            this.reason = reason;
+        }
+        public override string ToString()
+        {
+            return
+                $"id: {id} - " +
+                $"target_ID: {target_ID} - " +
+                $"created_at: {created_at} - " +
+                $"reason: {reason} - "
+                ;
+        }
     }
 }
